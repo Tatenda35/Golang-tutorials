2336e46b7df98c1565a29b1a615a01984e87052c
Tatenda <tatndor@gmail.com>
2025-04-19 18:31:27 +0200

    learning floats, intergers and formt Printf

--- Lesson03/intergers.go
+++ Lesson03/intergers.go
@@ -16,21 +16,30 @@ func main(){
 	ageOne = ageOne + 5
 	ageTwo = ageTwo + 5
 	ageThree = 5
-	ageFour = ageFour + 5
+	ageFour = ageFour + 5	
 
 	fmt.Println(ageOne, ageTwo, ageThree, ageFour)
 
-	gpa()
+	// bits and memory: integer type 8bits, 16bits etc
+	var numOne int8 = 127
+	var wordOne string = "Tatenda"
+	var length float32 = 1.5
+	decimalOne := 2.543
+	fmt.Println(numOne)
+	fmt.Printf("%s, got a gpa of %.1f, though he knows to count to %d, not to mention that he is %.2f tall!\n", wordOne, decimalOne, numOne, length)
+
+	// gpa()
 }
+
 
 // floats
 
-func gpa(){
-	var studentOne floats = 2.5
-	var studentTwo = 1.5
-	var studentThree floats
-	studentFour := 0.5
+// func gpa(){
+// 	var studentOne floats = 2.5
+// 	var studentTwo = 1.5
+// 	var studentThree floats
+// 	studentFour := 0.5
 
-	fmt.Println(studentOne, studentTwo, studentThree, studentFour)
+// 	fmt.Println(studentOne, studentTwo, studentThree, studentFour)
 
-}
+// }
