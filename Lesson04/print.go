c99ab31148252596f7fa22785a5ac12fdbd1d943
Tatenda <tatndor@gmail.com>
2025-04-20 06:18:51 +0200

    Various ways to format strings with Printf, q,T,v

--- Lesson04/print.go
+++ Lesson04/print.go
@@ -9,11 +9,24 @@ func main(){
 	height := 1.567
 	fmt.Print("Hello, ", nameOne,".\n")
 
-	// Printf is like in c programming where we can format the string and does not print a new line
-	fmt.Printf("Hello, %s. How does it feel to turn %d? And is it true that you are %.1fm tall.\n", nameOne, age, height)
-
 	// Println automatically prints a new line
 	// But an extra space is inserted after the variable
 	// So best to use Printf
 	fmt.Println("Hello,", nameOne,". How does it feel to turn,", age,"? And is it true that you are ", height, "m tall?")
+
+	// Printf is like in c programming where we can format the string and does not print a new line
+	// using %v for variable known as format specifier to represent where the variable will go
+	// or you can use %s for string, %d for int, %f for float
+	// %q puts qoutes
+	fmt.Printf("Hello, %v. How does it feel to turn %v? And is it true that you are %.1fm tall.\n", nameOne, age, height)
+	qoute := "By his stripes we were healed."
+	fmt.Printf("My favaourate qoute is, %q \n", qoute)
+
+	// getting the variable type
+	fmt.Printf("age is of type %T \n", age)
+
+	// printing floats
+	// percent sign is double %%
+	grade := 99.55
+	fmt.Printf("You scored %.1f%% \n", grade)
 }
